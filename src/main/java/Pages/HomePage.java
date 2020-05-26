545cb21d077f82cc159b46c3477b3b086a7fcd9b
noerm <noerm177@gmail.com>
2020-05-26 02:57:20 -0500

    Encontrar Textos

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,35 +1,31 @@
 package Pages;
 
+import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.List;
-
+import org.testng.Assert;
 
 public class HomePage {
 
     WebDriver driver;
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
-//Locates
+    // Locates
     @FindBy(css = "nav#uhf-g-nav > .js-paddle-items")
     @CacheLookup
     WebElement navMenuHome;
 
-    @FindBys
-    ( {
-            @FindBy(className = "single-link js-nav-menu uhf-menu-item"),
-            @FindBy(className = "single-link uhf-menu-item")
-    } )
-    public List<WebElement> navMenus;
+    @FindBys({ @FindBy(css = "a#shellmenu_1.c-uhf-nav-link"), @FindBy(id = "shellmenu_2"), @FindBy(id = "shellmenu_3"),
+            @FindBy(id = "shellmenu_4"), @FindBy(id = "shellmenu_5"), @FindBy(id = "l1_support") })
+    List<WebElement> navMenus; // = new ArrayList<WebElement>();
 
     @FindBy(css = "a#shellmenu_1.c-uhf-nav-link")
     WebElement officeText;
@@ -50,28 +46,76 @@ public class HomePage {
     WebElement supportText;
 
     public void getTextOffice() {
-        officeText.click();
-        
+        String realOffice = officeText.getAttribute("innerText");
+        try {
+            Assert.assertEquals("Office", realOffice);
+
+        } catch (AssertionError e) {
+            System.out.println("*ERROR* Expected: \"Office\" Found: " + realOffice);
+            throw e;
+        }
+        System.out.println("*PASS* Found: " + realOffice);
     }
 
-    public String getWinText(){
-        return  winText.getAttribute("innerHTML");
+    public void getWinText() {
+        String realWin = winText.getAttribute("innerText");
+        try {
+            Assert.assertEquals("Windows", realWin);
+
+        } catch (AssertionError e) {
+            System.out.println("*ERROR* Expected: \"Office\" Found: " + realWin);
+            throw e;
+        }
+        System.out.println("*PASS* Found: " + realWin);
     }
 
-    public String getSurfaceText(){
-        return  surfaceText.getAttribute("innerHTML");
+    public void getSurfaceText() {
+        String realSurf = surfaceText.getAttribute("innerText");
+        try {
+            Assert.assertEquals("Surface", realSurf);
+
+        } catch (AssertionError e) {
+            System.out.println("*ERROR* Expected: \"Office\" Found: " + realSurf);
+            throw e;
+        }
+        System.out.println("*PASS* Found: " + realSurf);
+
     }
 
-    public String getXboxText(){
-        return  xboxText.getAttribute("innerHTML");
+    public void getXboxText() {
+        String realXbx = xboxText.getAttribute("innerText");
+        try {
+            Assert.assertEquals("Xbox", realXbx);
+
+        } catch (AssertionError e) {
+            System.out.println("*ERROR* Expected: \"Office\" Found: " + realXbx);
+            throw e;
+        }
+        System.out.println("*PASS* Found: " + realXbx);
     }
 
-    public String getDealsText(){
-        return  dealsText.getAttribute("innerHTML");
+    public void getDealsText() {
+        String realDeal = dealsText.getAttribute("innerText");
+        try {
+            Assert.assertEquals("Deals", realDeal);
+
+        } catch (AssertionError e) {
+            System.out.println("*ERROR* Expected: \"Office\" Found: " + realDeal);
+            throw e;
+        }
+        System.out.println("*PASS* Found: " + realDeal);
     }
 
-    public String getSupportText(){
-        return  supportText.getAttribute("innerHTML");
+    public void getSupportText() {
+        String realSupp = supportText.getAttribute("innerText");
+        try {
+            Assert.assertEquals("Support", realSupp);
+
+        } catch (AssertionError e) {
+            System.out.println("*ERROR* Expected: \"Office\" Found: " + realSupp);
+            throw e;
+        }
+        System.out.println("*PASS* Found: " + realSupp);
     }
 
 }
--- src/test/java/tests/HomeStringsTest.java
+++ src/test/java/tests/HomeStringsTest.java
@@ -7,9 +7,16 @@ import org.testng.annotations.Test;
 public class HomeStringsTest extends TestBase {
 
     @Test
-    public void TestValues() throws Exception{
+    public void TestStringsOnMenu() throws Exception{
         HomePage HP = new HomePage(driver);
+        //System.out.println(HP.getTextOffice());
         HP.getTextOffice();
+        HP.getWinText();
+        HP.getSurfaceText();
+        HP.getXboxText();
+        HP.getDealsText();
+        HP.getSupportText();
+        
         //Assert.assertEquals(ActualValue,"Office");
     }
 
